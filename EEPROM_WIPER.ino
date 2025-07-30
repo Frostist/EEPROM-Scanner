d23427118f1e53a7fed74e190dbb289f0e2c6a62
Frostist <37545822+Frostist@users.noreply.github.com>
2025-07-30 14:21:21 +0200

    2.0.0

---
+++ EEPROM_WIPER.ino
@@ -0,0 +1,33 @@
+#include <EEPROM.h>
+#include <Arduino.h>
+
+//x defines the start area
+//y defines the end area
+
+int x = 0;
+int y = 250;
+
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  EEPROM.begin(y);
+
+  Serial.println("Starting EEPROM wipe...");
+  Serial.print("Wiping addresses 0 to ");
+  Serial.println(y - 1);
+
+  //Change the 250 to your desired end address
+  for (x = 0; x < y; x++) {
+    EEPROM.write(x, 0);  // Write 0 to each address
+    Serial.print("Wiped address ");
+    Serial.print(x);
+    Serial.println(": 0");
+  }
+
+  EEPROM.commit();  // Commit the changes to EEPROM
+  Serial.println("EEPROM wipe completed!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
